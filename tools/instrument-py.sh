a48147c238fd9d58d5b0aa68872ba954f8baa859
Shinhyung Yang <shinhyung.yang@email.uni-kiel.de>
2025-02-19 14:51:53 +0100

    Document the instrument-py.sh script

--- tools/instrument-py.sh
+++ tools/instrument-py.sh
@@ -1,4 +1,13 @@
 #/usr/bin/env bash
 
+# * Prepend the following line to all Python files of the target program.
+#
+#   from otkt.otelinit import tracer
+#
 sed -i '1 i\from otkt.instrument import instrument' **/*py
+
+# * Prepend the following line before all Python method definitions:
+#
+#   @instrument
+#
 sed -i 's/^\([\ \t]*\)\(def \)\(.*\)$/\1@instrument\n\1\2\3/g' **/*py
