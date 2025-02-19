8ab2f66036d16ac1a489a5aeab2f24e92afe5627
Shinhyung Yang <shinhyung.yang@email.uni-kiel.de>
2025-02-19 15:44:22 +0100

    More changes

--- tools/instrument-py.sh
+++ tools/instrument-py.sh
@@ -4,10 +4,10 @@
 #
 #   from otkt.otelinit import tracer
 #
-sed -i '1 i\from otkt.instrument import instrument' **/*py
+sed -i '1 i\from otkt.instrument import instrument' *.py **/*.py
 
 # * Prepend the following line before all Python method definitions:
 #
 #   @instrument
 #
-sed -i 's/^\([\ \t]*\)\(def \)\(.*\)$/\1@instrument\n\1\2\3/g' **/*py
+sed -i 's/^\([\ \t]*\)\(def \)\(.*\)$/\1@instrument\n\1\2\3/g' *.py **/*.py
