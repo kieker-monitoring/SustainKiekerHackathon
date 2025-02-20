ce9614521c0694446f86dbe8afde25c885505296
Shinhyung Yang <shinhyung.yang@email.uni-kiel.de>
2025-02-20 13:06:58 +0100

    Update instrument-py.sh

--- tools/instrument-py.sh
+++ tools/instrument-py.sh
@@ -4,10 +4,11 @@
 #
 #   from otkt.otelinit import tracer
 #
-for py in `find . -type f -name "*py"` ; do sed -i '1 i\from otkt.instrument import instrument' ${py} ; done
+
+for py in `find . -not -path '*/[@.]*' -type f -name "*py"` ; do sed -i '1 i\from otkt.instrument import instrument' ${py} ; done
 
 # * Prepend the following line before all Python method definitions:
 #
 #   @instrument
 #
-for py in `find . -type f -name "*py"` ; do sed -i 's/^\([\ \t]*\)\(def \)\(.*\)$/\1@instrument\n\1\2\3/g' ${py} ; done
+for py in `find . -not -path '*/[@.]*' -type f -name "*py"` ; do sed -i 's/^\([\ \t]*\)\(def \)\(.*\)$/\1@instrument\n\1\2\3/g' ${py} ; done
